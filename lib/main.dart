aa8650602e0e50ea7c9d7e675f5e50232ca1514f
pgmaximo <pedrogmaximo@gmail.com>
2024-03-27 08:12:14 -0300

    Primeiro commit Adicionando todos os arquivos, e o Main.dart, no qual já está começado

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+///////
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Meu Primeiro App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //Variaveis
+    const corRoxo = Color.fromRGBO(125, 96, 161, 1);
+
+    //Scaffold | Widgets
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomePage", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 40)),
+        backgroundColor : corRoxo,
+      ),
+      body: Center(
+        child: Column (
+          children: [
+            const Text(
+              'Every Purchase Will be Made With Pleasure',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35, )
+            ),
+            const Text(
+              'Buy and enjoy'
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(
+                  'Start shopping'
+                );
+              },
+              child: const Text('Iniciar compras'),
+            ),
+            children: Row [
+              ElevatedButton (
+                onPressed: () {
+                  print('Botão cliado')
+                }
+                child: const Text('botao 2')
+              )
+              ElevatedButton (
+                onPressed: () {
+                  print('Botão cliado')
+                }
+                child: const Text('botao 3'),
+              )
+            ]
+          ]
+        )
+      )
+    );
+  }
+}
